8cffe7a5e4d6974a48f3b34c7baeededf015df64
anandakarla019-hue <anandakarla019@gmail.com>
2026-02-21 12:45:49 +0000

    Aula 1

---
+++ hello.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void){
+    string nome;
+  cout << "Qual o seu nome? ";
+  cin >> nome;
+
+  cout << "olá, Quinn!" << endl;
+
+ return 0;
+}
